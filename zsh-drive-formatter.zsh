8fee0adf9649ad21775940c419532ecb2216bb7b
ricky <splixx05@gmail.com>
2025-10-19 01:29:45 +0200

    update vars in plugin init

--- zsh-drive-formatter.zsh
+++ zsh-drive-formatter.zsh
@@ -1,12 +1,13 @@
 # open a new tmux window for this function
 # otherwise the function will be run in the current terminal
 
-SRC="${ZSH:-$HOME/.zsh}/plugins/local/zsh-drive-formatter"
-
 function :format() {
+  local script_dir="${${(%):-%x}:A:h}"
+  local form_script="$script_dir/form.sh"
+
   if [[ -n $TMUX ]]; then
-    tmux new-window -n usbform "$SRC/form.sh"
+    tmux new-window -n usbform "$form_script"
   else
-    "$SRC/form.sh"
+    "$form_script"
   fi
 }
